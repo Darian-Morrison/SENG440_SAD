684cc33626c205cb2c5e2905842c672d75eea9eb
Darian Morrison <darianjmorrison@gmail.com>
2019-07-31 10:24:57 -0700

    Algorithm working on test bench

--- main.c
+++ main.c
@@ -1,10 +1,17 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
+#include <limits.h>
+#include <math.h>
 #include "bmp_file.h"
 
 unsigned char calculate_luminance(unsigned char red, unsigned char blue, unsigned char green);
 char sum_of_difference(unsigned char reference, unsigned char current);
+
+struct Motion_Vector
+{
+    int x, y;
+};
 
 int main(int argc, char *argv[]){
 
@@ -56,21 +63,59 @@ int main(int argc, char *argv[]){
     fclose(current_fp);
 
 //Perform algorithm
-    int sad= 0;
+    int sad, best_sad,current_y, current_x, reference_y, reference_x, displacement_y, displacement_x = 0;
     char diff;
+    struct Motion_Vector vector = {0,0};
 
-    for(i = 0; i < 16; i++){
-        for(j = 0; j < 16; j++){
-            diff = reference_luminance_pixles[i][j] - current_luminance_pixles[i][j];
-            if( diff < 0){
-                sad -= diff;
-            }else{
-                sad += diff;
+    struct Motion_Vector motion_array[reference_bmp.height/16][reference_bmp.width/16];
+
+//For each block of current 
+    for(current_y = 0; current_y < reference_bmp.height; current_y+=16){
+        for(current_x = 0; current_x <  reference_bmp.width; current_x+=16){
+            best_sad = INT_MAX;
+            //For every location in reference, find best_sad
+            for(reference_y = 0; reference_y < reference_bmp.height - 15; reference_y++){
+                for(reference_x = 0; reference_x <  reference_bmp.width - 15; reference_x++){
+                    displacement_x = current_x - reference_x;
+                    displacement_y = current_y - reference_y;
+                    vector = motion_array[current_y/16][current_x/16];
+                    //SAD
+                    sad = 0;
+                    for(i = 0; i < 16; i++){
+                        for(j = 0; j < 16; j++){
+                            diff = reference_luminance_pixles[reference_y + i][reference_x + j] - current_luminance_pixles[current_y + i][current_x + j];
+                            if( diff < 0){
+                                sad -= diff;
+                            }else{
+                                sad += diff;
+                            }
+                        }
+                    }
+
+                    //If best Sad replace motion array and update value
+                    if(sad == best_sad && abs(displacement_x) + abs(displacement_y) < abs(vector.x) + abs(vector.y)){
+                        motion_array[current_y/16][current_x/16].y = displacement_y;
+                        motion_array[current_y/16][current_x/16].x = displacement_x;
+                    }
+                    if(sad < best_sad){
+                        best_sad = sad;
+                        motion_array[current_y/16][current_x/16].y = displacement_y;
+                        motion_array[current_y/16][current_x/16].x = displacement_x;
+                    }
+
+                }
             }
         }
     }
 
-    printf("SAD Block 1=%d\n", sad);
+    for(i = 0; i < reference_bmp.height/16; i++){
+        for(j = 0; j < reference_bmp.width/16; j++){
+            if(motion_array[i][j].x != 0 || motion_array[i][j].y != 0){
+               printf("(Block (%d, %d) -> (%d, %d)\n", j, i, motion_array[i][j].x, motion_array[i][j].y ); 
+            }  
+        }
+    }
+//Sadd for a 16X16 block
 
     return 0;
 }
@@ -78,7 +123,7 @@ int main(int argc, char *argv[]){
 //((Red value X 299) + (Green value X 587) + (Blue value X 114)) / 1000
 unsigned char calculate_luminance(unsigned char red, unsigned char green, unsigned char blue){
 
-    unsigned int luminance = ((299 * red + 587 * green + blue * 114))/1000;
+    unsigned int luminance = ((299 * red + 0 * green + blue * 114))/1000;
     return (unsigned char) luminance;
 }
 
